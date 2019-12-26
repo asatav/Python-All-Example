f0e63a1f53107037a23bf4588ea2e8e6eb846b5a
ASHISH <ashishsatav968@gmail.com>
2019-12-26 15:27:53 +0530

    logger Demo1 Added

---
+++ Logger/Demo1.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(filename='log.txt',level=logging.WARNING)
+print("Python logging Demo")
+logging.debug("Debug massage")
+logging.info("info massage")
+logging.warning("warnning massage")
+logging.error("error massage")
+logging.critical("critical  massage")
